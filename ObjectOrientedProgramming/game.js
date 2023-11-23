1f91521b41d30b00889c85d58fe2ac57eceb0563
JADundee <Jadundee@icloud.com>
2023-11-23 09:02:27 -0700

    Refactored with Object.create()

--- ObjectOrientedProgramming/game.js
+++ ObjectOrientedProgramming/game.js
@@ -1,12 +1,34 @@
 // factory functions
+const elfFunctions = {
+    attack() {
+        return this.name + ' attacks with ' + this.weapon 
+    }
+}
+
 function createElf(name, weapon) {
     return {
         name,
         weapon,
-        attack() {
-            return name + ' attacks with ' + weapon 
-        }
     }
 }
 
 const Legoless = createElf('Legoless', 'Bow')
+Legoless.attack = elfFunctions.attack
+
+// Object.create() to clean it up
+
+const elfActionsStore = {
+    attack() {
+        return this.name + ' attacks with ' + this.weapon 
+    }
+}
+
+function createElf(name, weapon) {
+    let newElf = Object.create(elfActionsStore)
+        newElf.name = name
+        newElf.weapon = weapon
+        return newElf
+}
+
+const Legoless = createElf('Legoless', 'Bow')
+console.log(Legoless.attack())
