d5d229862f543b871b5d741dd951ec989381a003
JADundee <Jadundee@icloud.com>
2023-11-27 08:08:40 -0700

    caching adn memoization

--- FunctionalProgramming/currying.js
+++ FunctionalProgramming/currying.js
@@ -8,3 +8,27 @@ curriedMultiply(5)(6)(2)
 const multiply = (a, b, c) => a*b*c
 partialMultiply = multiply.bind(null, 5)
 partialMultiply(4, 10)
+
+
+
+//caching and memoization
+function addTo80(n) {
+    console.log('long')
+    return n + 80
+}
+
+
+function memoizedAddTo80(n) {
+    let cache = {}
+    return function(n){
+        if (n in cache) {
+            return cache[n]
+        } else {
+            console.log('')
+            cache[n] = n + 80
+            return cache[n]
+        }
+    }
+}
+
+const memoized = memoizedAddTo80()
