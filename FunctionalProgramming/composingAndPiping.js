287fb6bd249d4d787c266060c0acf6e01c9ff237
JADundee <Jadundee@icloud.com>
2023-11-27 08:30:39 -0700

    compose and pipe

---
+++ FunctionalProgramming/composingAndPiping.js
@@ -0,0 +1,18 @@
+//compose
+
+/* data --> function --> data --> function --> etc */
+
+const compose = (f, g) => (data) => f(g(data))
+const multiply = (num) => num*3
+const makePositive = (num) => Math.abs(num)
+const multiplyAbsolute = compose(multiply, makePositive)
+
+multiplyAbsolute(-50)
+
+//Pipe 
+const pipe = (f, g) => (data) => g(f(data))
+
+
+fn1(fn2(fn3(50)))
+compose(fn1, fn2, fn3)(50)
+pipe(fn3, fn2, fn1)(50)
