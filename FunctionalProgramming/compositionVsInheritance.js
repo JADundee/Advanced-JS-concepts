5b1cbd99335e76599be29d44ad75a20fc2ef7c78
JADundee <Jadundee@icloud.com>
2023-11-28 08:00:18 -0700

    Composition vs inheritance

---
+++ FunctionalProgramming/compositionVsInheritance.js
@@ -0,0 +1,18 @@
+//inheritance
+
+//tight coupling issue
+// coupling between child and parent class is tight form
+// can break child methods because parent class is slightly changed
+
+//fragile baseclass issue
+
+
+//hierarchy problem
+
+//how composition fixes these issues
+//changes a class from what it is to what it does
+
+
+//inheritance is a super class that extends to children classes that can overwrite or extend the super class
+
+// composition is smaller pieces to make desired output which makes it easy to add or remove pieces
