153a010e98fbb2e64b9188c23bd6de7c55957544
JADundee <Jadundee@icloud.com>
2023-12-02 06:06:28 -0700

    Creating an array from scratch

--- dataStructures/arrayStructures.js
+++ dataStructures/arrayStructures.js
@@ -15,3 +15,52 @@ array.splice(2, 0, 'y') // 0(n/2)
 
 //accessing array
 array[2] // 0(1)
+
+console.log(array)
+
+// static vs dynamic arrays
+//static arrays allocate memory beforehand with the set amount
+//dynamic allocate memory with automatic resizing
+
+
+//building an array from scratch
+
+
+class MyArray {
+    constructor() {
+        this.length = 0
+        this.data = {}
+    }
+
+    get(index) {
+        return this.data[index]
+    }
+
+    push(item) {
+        this.data[this.length] = item
+        this.length++
+        return this.length
+    }
+
+    pop() {
+        const lastItem = this.data[this.length-1]
+        delete this.data[this.length-1]
+        this.length--
+        return lastItem
+    }
+
+    delete(index) {
+        const item = this.data[index]
+        this.shiftItems(index)
+    }
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++){
+            this.data[i] = this.data[i + 1]
+        }
+       delete this.data[this.length - 1]
+       this.length--
+    }
+}
+
+const newArray = new MyArray()
+console.log(newArray)
